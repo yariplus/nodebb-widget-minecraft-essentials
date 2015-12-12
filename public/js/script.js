71fc697adbcafe01cd73306e3acfafeb4cb0a1ec
yariplus <tafike@gmail.com>
2015-12-11 23:23:33 -0500

    Testing navbar addon.

---
+++ public/js/script.js
@@ -0,0 +1,16 @@
+$(function(){
+	
+	$('<ul class="nav navbar-nav navbar-right"><div class="mw-container">\
+		<div>\
+			<span style="float: left; margin-right: 1rem; margin-top: 1rem;"><i style="font-size: 2.5rem;" class="fa\
+						fa-check-circle text-success\
+			"></i></span>\
+			<span style="float: right; margin-top: 0.325rem;">0/0</span>\
+			<span style="font-size: 1.8rem;">Server</span><br>\
+			<span style="opacity: 0.8;">example.com</span>\
+		</div>\
+		<div>\
+			\
+		</div>\
+	</div></ul>').insertBefore('#main-nav');
+});

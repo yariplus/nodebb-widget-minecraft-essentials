9992cf8fac97d763370881391f39ca6e169e5dae
yariplus <tafike@gmail.com>
2015-12-16 10:45:56 -0500

    Add icon to the navbar and make the script a bit more verbose.

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,21 @@
 $(function(){
 
 	if (config.MinecraftWidgets.navbarStatus) {
+
+		var icon;
+		if (config.MinecraftWidgets.isServerOnline) {
+			if (config.MinecraftWidgets.icon) {
+				icon = '<img src="'+config.MinecraftWidgets.icon+'" style="height: 46px; margin-right: 0.75em;">';
+			}else{
+				icon = '<span style="height: 25px; vertical-align: middle; display: inline-block; margin-right: 0.75em;"><i style="font-size: 2.5rem;" class="fa fa-check-circle text-success"></i></span>';
+			}
+		}else{
+			icon = '<span style="height: 25px; vertical-align: middle; display: inline-block; margin-right: 0.75em;"><i style="font-size: 2.5rem;" class="fa fa-times-circle text-danger"></i></span>';
+		}
+
 		$('<ul class="nav navbar-nav navbar-right"><div class="mw-container">'+
 			'<div style="height: 50px; display: table-cell; vertical-align: middle;" class="mw-container">'+
-			'<div style="display: inline-block; vertical-align: middle; margin-right: 0.5em;">'+
-				'<span style="height: 25px; vertical-align: middle; display: inline-block; margin-right: 0.5em;">'+
-					(config.MinecraftWidgets.isServerOnline ? '<i style="font-size: 2.5rem;" class="fa fa-check-circle text-success"></i>' : '<i style="font-size: 2.5rem;" class="fa fa-times-circle text-danger"></i>')+
-				'</span>'+
+			'<div style="display: inline-block; vertical-align: middle; margin-right: 0.5em;">'+icon+
 				'<span style="opacity: 0.8; display: inline-block; vertical-align: middle;">'+
 					'<div style="font-size: larger;">'+config.MinecraftWidgets.address+'</div>'+
 					'<div style="opacity: 0.6; font-size: smaller;">Players online: '+config.MinecraftWidgets.onlinePlayers+'</div>'+
